392dc698306cc88d53bf1927a6e06593d60eaae0
Hossein <smh.miraftabi1373@gmail.com>
2018-08-15 22:06:42 +0100

    rectifiedHttps

--- Hangman/scripts/requests.js
+++ Hangman/scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzle = async (wordCount) => {
-	const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+	const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`);
 	if (response.status === 200) {
 		const data = await response.json();
 		return data.puzzle;
@@ -14,7 +14,7 @@ const getCurrentCountry = async () => {
 };
 
 const getCountry = async (countryCode) => {
-	const response = await fetch('http://restcountries.eu/rest/v2/all');
+	const response = await fetch('//restcountries.eu/rest/v2/all');
 
 	if (response.status === 200) {
 		const data = await response.json();
@@ -25,7 +25,7 @@ const getCountry = async (countryCode) => {
 };
 
 const getLocation = async () => {
-	const response = await fetch('http://ipinfo.io/json?token=1a11bd55cc8f9c');
+	const response = await fetch('//ipinfo.io/json?token=1a11bd55cc8f9c');
 
 	if (response.status === 200) {
 		return response.json();
